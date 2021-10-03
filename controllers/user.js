d1aa8a20f8331bba1df4926ab0902ea9ca557bf3
Rochmad <rochmad.wa@gmail.com>
2021-10-03 07:56:43 +0700

    Implement checkout

---
+++ controllers/user.js
@@ -0,0 +1,74 @@
+const User = require('../models/user');
+const Cart = require('../models/cart');
+const Product = require('../models/product');
+
+exports.userCart = async (req, res) => {
+    const { cart } = req.body;
+
+    let products = [];
+
+    const user = await User.findOne({ email: req.user.email }).exec();
+
+    let cartExist = await Cart.findOne({ orderedBy: user._id }).exec();
+
+    if (cartExist) {
+        await cartExist.remove();
+    }
+
+    for (let i = 0; i < cart.length; i++) {
+        let object = {};
+
+        object.product = cart[i]._id;
+        object.count = cart[i].count;
+        object.color = cart[i].color;
+
+        let productFromDB = await Product.findById(cart[i]._id)
+            .select('price')
+            .exec();
+        object.price = productFromDB.price;
+
+        products.push(object);
+    }
+
+    let cartTotal = 0;
+    for (let i = 0; i < products.length; i++) {
+        cartTotal = cartTotal + products[i].price * products[i].count;
+    }
+
+    await new Cart({
+        products,
+        cartTotal,
+        orderedBy: user._id,
+    }).save();
+
+    res.json({ ok: true });
+};
+
+exports.getUserCart = async (req, res) => {
+    const user = await User.findOne({ email: req.user.email }).exec();
+
+    let cart = await Cart.findOne({ orderedBy: user._id }).populate(
+        'products.product',
+        '_id title price totalAfterDiscount'
+    ).exec();
+
+    const { products, cartTotal, totalAfterDiscount } = cart;
+
+    res.json({ products, cartTotal, totalAfterDiscount });
+};
+
+exports.emptyCart = async (req, res) => {
+    const user = await User.findOne({ email: req.user.email }).exec();
+
+    const cart = await Cart.findOneAndRemove({ orderedBy: user._id }).exec();
+
+    res.json(cart);
+}
+
+exports.saveAddress = async (req, res) => {
+    const userAddress = await User.findOneAndUpdate(
+        { email: req.user.email },
+        { address: req.body.address }
+    ).exec();
+    res.json({ ok: true });
+}
--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const { authCheck } = require('../middlewares/auth');
+const { userCart, getUserCart, emptyCart, saveAddress } = require('../controllers/user');
 
 const router = express.Router();
 
-router.get('/user', (req, res) => {
-    res.json('kboom');
-});
+router.post('/user/cart', authCheck, userCart);
+router.get('/user/cart', authCheck, getUserCart);
+router.delete('/user/cart', authCheck, emptyCart);
+router.post('/user/address', authCheck, saveAddress);
 
 module.exports = router;
